e246fa05d13b8eeab453cb56ad3b19c20a26ef8a
Documenter.jl <documenter@juliadocs.github.io>
2023-02-06 03:15:59 +0000

    build based on a6efa03

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SWCDatasets","category":"page"},{"location":"#SWCDatasets","page":"Home","title":"SWCDatasets","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SWCDatasets.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SWCDatasets]","category":"page"}]
+}
