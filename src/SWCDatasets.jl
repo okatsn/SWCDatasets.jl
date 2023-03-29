a73c27233302803c78b62efa6b19da30dc2421ab
okatsn <okatsn@gmail.com>
2023-03-29 09:58:56 +0800

    Add dataset Huashan

--- src/SWCDatasets.jl
+++ src/SWCDatasets.jl
@@ -1,14 +1,11 @@
 module SWCDatasets
 
-using DrWatson
-include("projectdir.jl")
-
 using SmallDatasetMaker # (required) See also `SmallDatasetMaker.datasets`.
 function SWCDatasets.dataset(package_name, dataset_name)
     SmallDatasetMaker.dataset(SWCDatasets,package_name, dataset_name)
 end # (optional)
 
-SWCDatasets.datasets() = SmallDatasetMaker.datasets(FSDatasets) # (optional)
-# so that you can use `FSDatasets.datasets()` to list all availabe `package/dataest`s in `FSDatasets`
+SWCDatasets.datasets() = SmallDatasetMaker.datasets(SWCDatasets) # (optional)
+# so that you can use `SWCDatasets.datasets()` to list all availabe `package/dataest`s in `SWCDatasets`
 
 end
