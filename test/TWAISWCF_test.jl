8478093734aeef77466d67add5ec89057288ce53
okatsn <okatsn@gmail.com>
2023-03-29 17:17:30 +0800

    TWAISWCF_test for testing the last added table

--- test/TWAISWCF_test.jl
+++ test/TWAISWCF_test.jl
@@ -1,6 +1,13 @@
 @testset "TWAISWCF_test.jl" begin
-    using TWAISWCF, SWCDatasets
+    using TWAISWCF
     lastrow = eachrow(SWCDatasets.__datasets) |> last
+    df = SWCDatasets.dataset(lastrow.PackageName, lastrow.Dataset)
+    PT = PrepareTableDefault(df) # data preprocessing
+    traintest!(PT;
+            train_before = DateTime(2022, 03, 21),
+            test_after = DateTime(2022, 3, 22)) # train and test
+    save(PT)
+
     # CHECKPOINT: test if lastrow can be loaded and PT
     # CHECKPOINT: move raw data to test and test if last dataset is identical than the raw one
 
