0f726281257dad1f5f4e7c520a564c634a055355
okatsn <okatsn@gmail.com>
2023-03-30 14:14:49 +0800

    Test if the last added dataset can go through default preprocessing

--- test/TWAISWCF_test.jl
+++ test/TWAISWCF_test.jl
@@ -1,14 +1,7 @@
 @testset "TWAISWCF_test.jl" begin
-    using TWAISWCF
-    lastrow = eachrow(SWCDatasets.__datasets) |> last
+    using TWAISWCF, DataFrames
+    lastrow = eachrow(SWCDatasets.datasets()) |> last
     df = SWCDatasets.dataset(lastrow.PackageName, lastrow.Dataset)
     PT = PrepareTableDefault(df) # data preprocessing
-    traintest!(PT;
-            train_before = DateTime(2022, 03, 21),
-            test_after = DateTime(2022, 3, 22)) # train and test
-    save(PT)
-
-    # CHECKPOINT: test if lastrow can be loaded and PT
-    # CHECKPOINT: move raw data to test and test if last dataset is identical than the raw one
-
+    @test true
 end
