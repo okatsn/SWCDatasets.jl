96a26c698109b2551cb81e089201d1c771155c89
okatsn <okatsn@gmail.com>
2023-04-10 12:30:06 +0800

    Making NCU tower dataset

---
+++ scripts/making_TowerNCU.jl
@@ -0,0 +1,19 @@
+using DataFrames
+using CSV
+using Revise, SmallDatasetMaker, SWCDatasets, OkFiles
+
+# ## Load the tables
+fpaths = filelistall(r"TowerNCU", SWCDatasets.dir_raw())
+dfs = CSV.read.(fpaths, DataFrame)
+basename.(fpaths)
+
+# ## Table differences ignoring `soil_water_`
+dft = difftables(dfs...; ignoring = Cols(r"soil_water_"))
+
+
+# ## Remove extra soil water contents
+select!.(dfs, [Not(Cols(r"soil_water_content_\D+"))]) # Delete e.g., soil_water_content_#2...
+select!.(dfs, [Not(Cols(r"soil_water_content_\d+\_"))]) # Delete e.g., soil_water_content_2...
+dft = difftables(dfs...)
+
+# ## Outer join the tables
